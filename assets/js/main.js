27599fe4b3955df16d6807221e728d963fc59124
unknown <vagnerrsimango@gmail.com>
2023-04-02 22:56:50 +0200

    changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,48 +1,53 @@
 // Scrollspy
-var spy = new Gumshoe('nav a');
+var spy = new Gumshoe("nav a");
 // Burger
-    $('.burger').on('click', function (event) {
-        $('.navigation-bar').slideToggle('200');
-        $('.toggle-open').toggleClass('open');
-        $('.toggle-close').toggleClass('close');
-    })
+$(".burger").on("click", function (event) {
+  $(".navigation-bar").slideToggle("200");
+  $(".toggle-open").toggleClass("open");
+  $(".toggle-close").toggleClass("close");
+});
 
 // Sticky navbar
-$(document).ready(function(){
-	$(window).bind('scroll', function() {
-		var navHeight = $('header').height();
-		if ($(window).scrollTop() > navHeight) {
-			$('header').addClass('fixed');
-		 }
-		else {
-			$('header').removeClass('fixed');
-		 }
-	});
+$(document).ready(function () {
+  $(window).bind("scroll", function () {
+    var navHeight = $("header").height();
+    if ($(window).scrollTop() > navHeight) {
+      $("header").addClass("fixed");
+    } else {
+      $("header").removeClass("fixed");
+    }
+  });
 });
 // Scroll to TOP
-var btn = $('.scrollup');
-$(window).scroll(function() {
+var btn = $(".scrollup");
+$(window).scroll(function () {
   if ($(window).scrollTop() > 500) {
-    btn.addClass('show');
+    btn.addClass("show");
   } else {
-    btn.removeClass('show');
+    btn.removeClass("show");
   }
 });
-btn.on('click', function(e) {
+btn.on("click", function (e) {
   e.preventDefault();
-  $('html, body').animate({scrollTop:0}, '500');
+  $("html, body").animate({ scrollTop: 0 }, "500");
 });
-    // Reveal animation
-    const sr = ScrollReveal({
-        origin: 'top',
-        distance: '20px',
-        duration: 1500,
-        reset: true
-    });
-    ScrollReveal().reveal('.col, .aboutus-img, .img-col', { origin: 'left' });
-    ScrollReveal().reveal('.aboutus-content, .home-image', { origin: 'right' });
-    ScrollReveal().reveal('.product-item, .brand-wrapper, .newsletter-wrapper', { interval: 100 });
-   
+// Reveal animation
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "20px",
+  duration: 1500,
+  reset: true,
+});
+ScrollReveal().reveal(".col, .aboutus-img, .img-col", { origin: "left" });
+ScrollReveal().reveal(".aboutus-content, .home-image", { origin: "right" });
+ScrollReveal().reveal(".product-item, .brand-wrapper, .newsletter-wrapper", {
+  interval: 100,
+});
 
-
-
+let slideIndex = 0;
+const images = document.querySelectorAll(".slideshow-image");
+setInterval(() => {
+  images[slideIndex].classList.remove("active");
+  slideIndex = (slideIndex + 1) % images.length;
+  images[slideIndex].classList.add("active");
+}, 3000);
